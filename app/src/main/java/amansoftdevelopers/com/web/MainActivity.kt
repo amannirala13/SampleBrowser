dc5b1eba7315fdb2774d5763627f04602315e2c0
Aman Kumar Nirala <35570939+amannirala13@users.noreply.github.com>
2018-03-12 08:57:37 +0530

    Sample

--- app/src/main/java/amansoftdevelopers/com/web/MainActivity.kt
+++ app/src/main/java/amansoftdevelopers/com/web/MainActivity.kt
@@ -1,5 +1,8 @@
 package amansoftdevelopers.com.web
 
+import amansoftdevelopers.com.web.R.id.txtUrl
+import android.annotation.TargetApi
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.webkit.URLUtil
@@ -8,16 +11,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @TargetApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        wv.webViewClient
         btn.setOnClickListener({
             wv.loadUrl(
                     if(URLUtil.isValidUrl(txtUrl.text.toString()))
             txtUrl.text.toString()
             else
                 "http://"+txtUrl.text.toString())
+
         })
     }
 }
